e32be2a6247e1859e4c7c506911b3d43b8c59855
Prince Stevie-Ray Charles Balabis <princebalabis@gmail.com>
2014-05-20 17:52:59 +0200

    Changed priorities of threads

--- Software/HoverCraftSystem/MainThread.ino
+++ Software/HoverCraftSystem/MainThread.ino
@@ -3,20 +3,15 @@ void mainThread() {
 
 	Serial.begin(9600); // Start Serial
 
-	chThdCreateStatic(fr_LineSensorSystem, sizeof(fr_LineSensorSystem),
-		NORMALPRIO, Thread1, NULL);
+	chThdCreateStatic(fr_LineSensorSystem, sizeof(fr_LineSensorSystem), NORMALPRIO + 3, Thread1, NULL);
 
-	chThdCreateStatic(fr_LeftmotorPID, sizeof(fr_LeftmotorPID),
-		NORMALPRIO, Thread2, NULL);
+	chThdCreateStatic(fr_LeftmotorPID, sizeof(fr_LeftmotorPID), NORMALPRIO + 2, Thread2, NULL);
 
-	chThdCreateStatic(fr_RightmotorPID, sizeof(fr_RightmotorPID),
-		NORMALPRIO, Thread3, NULL);
+	chThdCreateStatic(fr_LeftmotorControl, sizeof(fr_LeftmotorControl), NORMALPRIO + 1, Thread4, NULL);
 
-	chThdCreateStatic(fr_LeftmotorControl, sizeof(fr_LeftmotorControl),
-		NORMALPRIO, Thread4, NULL);
+	//chThdCreateStatic(fr_RightmotorPID, sizeof(fr_RightmotorPID), NORMALPRIO, Thread3 + 2, NULL);
 
-	chThdCreateStatic(fr_RightmotorControl, sizeof(fr_RightmotorControl),
-		NORMALPRIO, Thread5, NULL);
+	//chThdCreateStatic(fr_RightmotorControl, sizeof(fr_RightmotorControl) + 1, NORMALPRIO, Thread5, NULL);
 
 
 
