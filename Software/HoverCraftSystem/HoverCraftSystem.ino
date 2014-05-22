67856a603fd3b61d41ecac3069264f73e2d612df
Prince Stevie-Ray Charles Balabis <princebalabis@gmail.com>
2014-05-22 23:34:44 +0200

    Automatically created files

---
+++ Software/HoverCraftSystem/HoverCraftSystem.ino
@@ -0,0 +1,69 @@
+#include <FreeRTOS_ARM.h>
+#include <PID_v1.h>
+
+extern double* pfr_FRMPID;
+extern double* pfr_FLMPID;
+
+extern double* pbr_BRMPID;
+extern double* pbr_BLMPID;
+
+extern double* pFLMControl;
+extern double* pFRMControl;
+
+extern double* pBLMControl;
+extern double* pBRMControl;
+
+extern int runFrontCalibration = 0;
+extern int runRearCalibration = 0;
+extern int startDriving = 0;
+
+//Buzzer-beep Semaphore
+//xSemaphoreHandle buzzerSem;
+//extern double* pbuzzerSem = &buzzerSem;
+
+void setup() {
+	// Start serial
+	Serial.begin(38400);
+
+	//Makes sure calibration-programs doesnt run automatically at start
+	runFrontCalibration = 0;
+	runRearCalibration = 0;
+	startDriving = 0;
+
+	// Tasks for front systems
+	xTaskCreate(Thread1, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL); //FrontLineSensorSystem
+	xTaskCreate(Thread3, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL); //FrontLeftMotorPID
+	xTaskCreate(Thread4, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL); //FrontRIghtMotorPID
+	xTaskCreate(Thread7, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL); //FrontLeftMotorControl
+	xTaskCreate(Thread8, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL); //FrontRightMotorControl
+
+	// Tasks for rear systems
+	xTaskCreate(Thread2, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL); //RearLineSensorSystem
+	xTaskCreate(Thread5, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL); //RearLeftMotorPID
+	xTaskCreate(Thread6, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL); //RearRightMotorPID
+	xTaskCreate(Thread9, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL); //RearLeftMotorControl
+	xTaskCreate(Thread10, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL); //RearRightMotorControl
+
+	//Tasks for forwardPropulsionFan
+	//xTaskCreate(Thread11, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL); //LiftFanMotorControl
+
+	//Tasks for Liftfan
+	//xTaskCreate(Thread12, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL); //LiftFanMotorControl
+
+	//Tasks for additional functions
+	//xTaskCreate(Thread13, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL); //Buzzer
+	xTaskCreate(Thread14, NULL, configMINIMAL_STACK_SIZE, NULL, 2, NULL); //BluetoothModule
+
+
+	// start scheduler
+	vTaskStartScheduler();
+
+	Serial.println(F("Insufficient RAM"));
+
+	while (1);
+}
+
+void loop() {
+	// Not used
+
+}
---
+++ Software/HoverCraftSystem/RearLeftMotorPID.ino
@@ -0,0 +1,30 @@
+//Define Variables we'll be connecting to
+double br_SetpointLeft = 8;
+
+double BLMControl;
+double* pBLMControl = &FLMControl;
+
+//Specify the links and initial tuning parameters
+PID br_myPIDLeft(pbr_BLMPID, pBLMControl, &br_SetpointLeft, 25, 0, 0, DIRECT);
+
+static void Thread5(void *arg) {
+
+	//turn the PID on
+	br_myPIDLeft.SetMode(AUTOMATIC);
+
+	br_myPIDLeft.SetControllerDirection(REVERSE);
+	br_myPIDLeft.SetOutputLimits(40, 225);
+
+	while (1){
+		br_myPIDLeft.Compute();
+		//Serial.print(*pbr_BLMPID);
+		//Serial.print(" - ");
+		//Serial.print(*pBLMControl);
+		//Serial.print(" - ");
+		//Serial.print(*pbr_BRMPID);
+		//Serial.print(" - ");
+		//Serial.print(*pBRMControl);
+		//Serial.println();
+	}
+}
+
