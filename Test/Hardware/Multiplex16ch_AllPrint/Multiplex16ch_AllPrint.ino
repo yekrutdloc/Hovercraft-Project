dbc71e35e4e58528bca8508baf395121009456d4
Prince Stevie-Ray Charles Balabis <princebalabis@gmail.com>
2014-05-22 17:28:19 +0200

    Added multiplex files

---
+++ Test/Hardware/Multiplex16ch_AllPrint/Multiplex16ch_AllPrint.ino
@@ -0,0 +1,183 @@
+// This program is made to test a 16 channel multiplex.
+// Connect LEDs to each channel & 5V to common Input/Output on the multiplex
+// Choosing the channel is done by writing to Serial.
+// Enter a number between 0-15 to choose a channel accordingly.
+
+// Multiplex pins
+int const s0pin = 53;
+int const s1pin = 52;
+int const s2pin = 51;
+int const s3pin = 50;
+
+void setup() {
+  Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+
+  // Multiplex setup
+  pinMode(s0pin, OUTPUT);    // s0
+  pinMode(s1pin, OUTPUT);    // s1
+  pinMode(s2pin, OUTPUT);    // s2
+  pinMode(s3pin, OUTPUT);    // s3
+}
+
+void loop() {
+  // Read the serial message and convert it to an integer
+
+  lightLEDChannel();
+  Serial.println();
+  delay(100);
+  // Print out the chosen channel
+}
+
+//This function takes in a channel number, and uses a case switch to alert the multiplex
+// which channel is wanted. The truth table is converted from the datasheet of the multiplex.
+void lightLEDChannel() {
+  for (int i = 0; i < 16; i++) {
+    switch (i) {
+      case 0:
+        digitalWrite(s0pin, LOW);
+        digitalWrite(s1pin, LOW);
+        digitalWrite(s2pin, LOW);
+        digitalWrite(s3pin, LOW);
+        Serial.print(analogRead(1));
+        Serial.print("-");
+        break;
+      case 1:
+        digitalWrite(s0pin, HIGH);
+        digitalWrite(s1pin, LOW);
+        digitalWrite(s2pin, LOW);
+        digitalWrite(s3pin, LOW);
+        Serial.print(analogRead(1));
+        Serial.print("-");
+
+        break;
+      case 2:
+        digitalWrite(s0pin, LOW);
+        digitalWrite(s1pin, HIGH);
+        digitalWrite(s2pin, LOW);
+        digitalWrite(s3pin, LOW);
+        Serial.print(analogRead(1));
+        Serial.print("-");
+
+        break;
+      case 3:
+        digitalWrite(s0pin, HIGH);
+        digitalWrite(s1pin, HIGH);
+        digitalWrite(s2pin, LOW);
+        digitalWrite(s3pin, LOW);
+        Serial.print(analogRead(1));
+        Serial.print("-");
+
+        break;
+      case 4:
+        digitalWrite(s0pin, LOW);
+        digitalWrite(s1pin, LOW);
+        digitalWrite(s2pin, HIGH);
+        digitalWrite(s3pin, LOW);
+        Serial.print(analogRead(1));
+        Serial.print("-");
+
+        break;
+      case 5:
+        digitalWrite(s0pin, HIGH);
+        digitalWrite(s1pin, LOW);
+        digitalWrite(s2pin, HIGH);
+        digitalWrite(s3pin, LOW);
+        Serial.print(analogRead(1));
+        Serial.print("-");
+
+        break;
+      case 6:
+        digitalWrite(s0pin, LOW);
+        digitalWrite(s1pin, HIGH);
+        digitalWrite(s2pin, HIGH);
+        digitalWrite(s3pin, LOW);
+        Serial.print(analogRead(1));
+        Serial.print("-");
+
+        break;
+      case 7:
+        digitalWrite(s0pin, HIGH);
+        digitalWrite(s1pin, HIGH);
+        digitalWrite(s2pin, HIGH);
+        digitalWrite(s3pin, LOW);
+        Serial.print(analogRead(1));
+        Serial.print("-");
+
+        break;
+      case 8:
+        digitalWrite(s0pin, LOW);
+        digitalWrite(s1pin, LOW);
+        digitalWrite(s2pin, LOW);
+        digitalWrite(s3pin, HIGH);
+        Serial.print(analogRead(1));
+        Serial.print("-");
+
+        break;
+      case 9 :
+        digitalWrite(s0pin, HIGH);
+        digitalWrite(s1pin, LOW);
+        digitalWrite(s2pin, LOW);
+        digitalWrite(s3pin, HIGH);
+        Serial.print(analogRead(1));
+        Serial.print("-");
+
+
+        break;
+      case 10:
+        digitalWrite(s0pin, LOW);
+        digitalWrite(s1pin, HIGH);
+        digitalWrite(s2pin, LOW);
+        digitalWrite(s3pin, HIGH);
+        Serial.print(analogRead(1));
+        Serial.print("-");
+
+        break;
+      case 11:
+        digitalWrite(s0pin, HIGH);
+        digitalWrite(s1pin, HIGH);
+        digitalWrite(s2pin, LOW);
+        digitalWrite(s3pin, HIGH);
+        Serial.print(analogRead(1));
+        Serial.print("-");
+
+        break;
+      case 12:
+        digitalWrite(s0pin, LOW);
+        digitalWrite(s1pin, LOW);
+        digitalWrite(s2pin, HIGH);
+        digitalWrite(s3pin, HIGH);
+        Serial.print(analogRead(1));
+        Serial.print("-");
+
+        break;
+      case 13:
+        digitalWrite(s0pin, HIGH);
+        digitalWrite(s1pin, LOW);
+        digitalWrite(s2pin, HIGH);
+        digitalWrite(s3pin, HIGH);
+        Serial.print(analogRead(1));
+        Serial.print("-");
+
+        break;
+      case 14:
+        digitalWrite(s0pin, LOW);
+        digitalWrite(s1pin, HIGH);
+        digitalWrite(s2pin, HIGH);
+        digitalWrite(s3pin, HIGH);
+        Serial.print(analogRead(1));
+        Serial.print("-");
+
+        break;
+      case 15:
+        digitalWrite(s0pin, HIGH);
+        digitalWrite(s1pin, HIGH);
+        digitalWrite(s2pin, HIGH);
+        digitalWrite(s3pin, HIGH);
+        Serial.print(analogRead(1));
+        Serial.print("-");
+
+        break;
+    }
+  }
+}
+
---
+++ Test/Hardware/Multiplex1ch_PhotoresistorChoosePrint/Multiplex1ch_PhotoresistorChoosePrint.ino
@@ -0,0 +1,136 @@
+
+// Front Line-Sensor pins
+int const s0pin = 53;
+int const s1pin = 52;
+int const s2pin = 51;
+int const s3pin = 50;
+int const z = A1;
+
+void setup() {
+  Serial.begin(9600);
+
+  // Photo resistor array setup
+  pinMode(s0pin, OUTPUT);    // s0
+  pinMode(s1pin, OUTPUT);    // s1
+  pinMode(s2pin, OUTPUT);    // s2
+  pinMode(s3pin, OUTPUT);    // s3
+
+  // Read the serial message and convert it to an integer
+  char buffer[] = {' ', ' ', ' ', ' ', ' ', ' ', ' '}; // Receive up to 7 bytes
+    while (!Serial.available()); // Wait for characters
+  Serial.readBytesUntil('\n', buffer, 7);
+  int incomingValue = atoi(buffer);
+
+  lightLEDChannel(incomingValue);
+}
+
+void loop() {
+  Serial.println(analogRead(z));
+  delay(200);
+}
+
+
+//This function takes in a channel number, and uses a case switch to alert the multiplex
+// which channel is wanted. The truth table is converted from the datasheet of the multiplex.
+void lightLEDChannel(int photoChannel) {
+  switch (photoChannel) {
+    case 0:
+      digitalWrite(s0pin, LOW);
+      digitalWrite(s1pin, LOW);
+      digitalWrite(s2pin, LOW);
+      digitalWrite(s3pin, LOW);
+      break;
+    case 1:
+      digitalWrite(s0pin, HIGH);
+      digitalWrite(s1pin, LOW);
+      digitalWrite(s2pin, LOW);
+      digitalWrite(s3pin, LOW);
+      break;
+    case 2:
+      digitalWrite(s0pin, LOW);
+      digitalWrite(s1pin, HIGH);
+      digitalWrite(s2pin, LOW);
+      digitalWrite(s3pin, LOW);
+      break;
+    case 3:
+      digitalWrite(s0pin, HIGH);
+      digitalWrite(s1pin, HIGH);
+      digitalWrite(s2pin, LOW);
+      digitalWrite(s3pin, LOW);
+      break;
+    case 4:
+      digitalWrite(s0pin, LOW);
+      digitalWrite(s1pin, LOW);
+      digitalWrite(s2pin, HIGH);
+      digitalWrite(s3pin, LOW);
+      break;
+    case 5:
+      digitalWrite(s0pin, HIGH);
+      digitalWrite(s1pin, LOW);
+      digitalWrite(s2pin, HIGH);
+      digitalWrite(s3pin, LOW);
+      break;
+    case 6:
+      digitalWrite(s0pin, LOW);
+      digitalWrite(s1pin, HIGH);
+      digitalWrite(s2pin, HIGH);
+      digitalWrite(s3pin, LOW);
+      break;
+    case 7:
+      digitalWrite(s0pin, HIGH);
+      digitalWrite(s1pin, HIGH);
+      digitalWrite(s2pin, HIGH);
+      digitalWrite(s3pin, LOW);
+      break;
+    case 8:
+      digitalWrite(s0pin, LOW);
+      digitalWrite(s1pin, LOW);
+      digitalWrite(s2pin, LOW);
+      digitalWrite(s3pin, HIGH);
+      break;
+    case 9 :
+      digitalWrite(s0pin, HIGH);
+      digitalWrite(s1pin, LOW);
+      digitalWrite(s2pin, LOW);
+      digitalWrite(s3pin, HIGH);
+
+      break;
+    case 10:
+      digitalWrite(s0pin, LOW);
+      digitalWrite(s1pin, HIGH);
+      digitalWrite(s2pin, LOW);
+      digitalWrite(s3pin, HIGH);
+      break;
+    case 11:
+      digitalWrite(s0pin, HIGH);
+      digitalWrite(s1pin, HIGH);
+      digitalWrite(s2pin, LOW);
+      digitalWrite(s3pin, HIGH);
+      break;
+    case 12:
+      digitalWrite(s0pin, LOW);
+      digitalWrite(s1pin, LOW);
+      digitalWrite(s2pin, HIGH);
+      digitalWrite(s3pin, HIGH);
+      break;
+    case 13:
+      digitalWrite(s0pin, HIGH);
+      digitalWrite(s1pin, LOW);
+      digitalWrite(s2pin, HIGH);
+      digitalWrite(s3pin, HIGH);
+      break;
+    case 14:
+      digitalWrite(s0pin, LOW);
+      digitalWrite(s1pin, HIGH);
+      digitalWrite(s2pin, HIGH);
+      digitalWrite(s3pin, HIGH);
+      break;
+    case 15:
+      digitalWrite(s0pin, HIGH);
+      digitalWrite(s1pin, HIGH);
+      digitalWrite(s2pin, HIGH);
+      digitalWrite(s3pin, HIGH);
+      break;
+  }
+}
+
